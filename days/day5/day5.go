4b7e2fd983f627d65c88700a2557ef353cc6feed
Bjarne Braeuer <bjarne.braeuer@parshipgroup.com>
2024-12-11 11:48:19 +0100

    day5 go part

--- days/day5/day5.go
+++ days/day5/day5.go
@@ -3,6 +3,7 @@ package day5
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -19,18 +20,18 @@ func (instance *Sortable) Equals(other *Sortable) bool {
 	return instance.Index == other.Index
 }
 
-func (instance *Sortable) LessThan(other *Sortable, rules map[int]int) bool {
-	_, exists := rules[instance.Index]
+func (instance *Sortable) LessThan(other *Sortable, rules map[int]struct{}) bool {
+	_, exists := rules[other.Index]
 	return !exists
 }
 
-func DataParsing(input string) (map[int]int, string) {
+func DataParsing(input string) (map[int]struct{}, string) {
 	parts := strings.Split(input, "\n\n")
 
 	rulesData := parts[0]
 	updates := parts[1]
 
-	rules := make(map[int]int)
+	rules := make(map[int]struct{})
 	for _, line := range strings.Split(rulesData, "\n") {
 		if line == "" {
 			continue
@@ -38,8 +39,7 @@ func DataParsing(input string) (map[int]int, string) {
 
 		parts := strings.Split(line, "|")
 		key, _ := strconv.Atoi(parts[0])
-		value, _ := strconv.Atoi(parts[1])
-		rules[key] = value
+		rules[key] = struct{}{}
 	}
 
 	return rules, updates
@@ -49,22 +49,48 @@ func splitLines(input string) []string {
 	return strings.Split(input, "\n")
 }
 
-func SortableFromUpdatesList(updatesList string) {
+func SortableFromUpdatesList(updatesList string, rules map[int]struct{}) (int, int) {
 	lines := splitLines(updatesList)
+	partA, partB := 0, 0
+
 	for _, line := range lines {
-		parts := strings.Split(line, ", ")
+		parts := strings.Split(line, ",")
 		var unsortedUpdate []*Sortable
 		for _, part := range parts {
 			index, err := strconv.Atoi(part)
 			if err != nil {
 				fmt.Println(err)
-				return
+				return 0, 0
 			}
 			unsortedUpdate = append(unsortedUpdate, NewSortable(index))
 		}
 
-		// TODO: sort here the unsortedUpdate and check cases (length / 2) & also return value
+		sortedUpdate := make([]*Sortable, len(unsortedUpdate))
+		copy(sortedUpdate, unsortedUpdate)
+		sort.Slice(sortedUpdate, func(i, j int) bool {
+			return sortedUpdate[i].LessThan(sortedUpdate[j], rules)
+		})
+
+		if isEqual(unsortedUpdate, sortedUpdate) {
+			partA += sortedUpdate[len(sortedUpdate)/2].Index
+		} else {
+			partB += sortedUpdate[len(sortedUpdate)/2].Index
+		}
 	}
+
+	return partA, partB
+}
+
+func isEqual(a, b []*Sortable) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if !a[i].Equals(b[i]) {
+			return false
+		}
+	}
+	return true
 }
 
 func RunDay5() {
@@ -76,8 +102,6 @@ func RunDay5() {
 	inputString := string(input)
 	rules, updates := DataParsing(inputString)
 
-	fmt.Println(rules)
-	fmt.Println(updates)
-
-	SortableFromUpdatesList(updates)
+	partA, partB := SortableFromUpdatesList(updates, rules)
+	fmt.Println(partA, partB)
 }
